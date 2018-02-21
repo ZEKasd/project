c9a1f59440cd77923cb9dc95bed80457fadf0a13
Viktor Balashov <zek506viktor@gmail.com>
2018-02-21 09:44:47 +0200

    Initial commit

---
+++ src/asd.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class asd {
+    public static void main(String[] args)
+    {
+        String s ="123"; String st="123.456";
+int x;double y;
+x=Integer.parseInt(s);
+y=Double.parseDouble(st);
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        s=String.valueOf(x+1);
+        st=String.valueOf(y+1);
+        System.out.println("strInt="+s);
+        System.out.println("strDouble="+st);
+        String str;
+        str="num"+345;
+        System.out.println(str);
+    }
+    }
